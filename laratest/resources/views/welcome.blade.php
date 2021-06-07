cb58c3e1a17067c2e39373e4b2b30461ca6cbfe3
Shahrier Hossain <hossainshahrier.shm@gmail.com>
2021-06-07 12:48:17 +0600

    Initial

---
+++ laratest/resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Social Media</title>
+
+</head>
+
+<body>
+    <div class="navbar">
+        <nav>
+            <div class="logo">
+                <p>Logo</p>
+            </div>
+            <div class="account">
+                <ul class="account_list">
+                    <li><a href="login">Login</a></li>
+                    <li><a href="register">Register</a></li>
+                </ul>
+
+            </div>
+        </nav>
+    </div>
+</body>
+
+</html>
